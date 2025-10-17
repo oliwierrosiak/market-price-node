c5c03c697a284fc7edf0b0b37077e57b42849fa4
Oliwier <oliwierrosiak07@gmail.com>
2025-10-17 11:10:37 +0200

    Currency exchange added

--- controllers/getEtf.js
+++ controllers/getEtf.js
@@ -1,34 +1,67 @@
 import axios from "axios"
 
+const exchangeRat = async(target) =>
+{
+    try
+    {
+        const resp = await axios.get(`https://api.frankfurter.app/latest?from=USD&to=${target}`)
+        return resp.data.rates[target]
+    }
+    catch(ex)
+    {
+        console.log(ex)
+        return null
+    }
+}
+
 async function getETF(req,res)
 {
 
-    const responseObject = []
-    const symbols = ['SPY','IVV','VOO','VTI','QQQ','EFA','EEM','VEA','VWO','XLK','XLF','XLE','XLV','XLY','GLD','IAU','SLV','USO','TLT','IEF','ARKK','SOXX','SMH','BOTZ','SPXS.L','VUAA.L','EQQQ.L','ISF.L']
-    for(let i = 0;i<symbols.length;i++)
+    const symbols = ['SPY','IVV','VOO','VTI','QQQ','EFA','EEM','VEA','VWO','XLK','XLF','XLE','XLV','XLY','GLD','IAU','SLV','USO','TLT','IEF','ARKK','SOXX','SMH','BOTZ','SPXS.L','VUAA.L','EQQQ.L','ISF.L','ITOT','IWM','VB','VTV','VUG','SCHD','BND','DIA','IXUS','VXUS','VEU','XLP','XLI','QQQM']
+    const promises = symbols.map(x=>
     {
-        try
-        {
-            const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${symbols[i]}?range=7d&interval=1d`)
+        return axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${x}?range=7d&interval=1d`)
+        .then(response=>{
             const last7d=response.data.chart.result[0].indicators.quote[0].close
             const clearResponse = response.data.chart.result[0].meta
-            responseObject.push({id:clearResponse.symbol,name:clearResponse.longName,currentPrice:clearResponse.regularMarketPrice,priceChange:clearResponse.regularMarketPrice-last7d.at(-2),percentPriceChange:(clearResponse.regularMarketPrice - last7d.at(-2))/last7d.at(-2)*100,sparkline:last7d})
-        }
-        catch(ex)
-        {
-        }
+            return{
+                id:clearResponse.symbol,
+                name:clearResponse.longName,
+                currentPrice:clearResponse.regularMarketPrice,
+                priceChange:clearResponse.regularMarketPrice-last7d.at(-2),
+                percentPriceChange:(clearResponse.regularMarketPrice - last7d.at(-2))/last7d.at(-2)*100,
+                sparkline:last7d}
+            }).catch(()=>null)
+
     }
-    if(responseObject.length)
+    )
+    const object = await Promise.all(promises)
+    const responseObject = object.filter(x=> x !== null)
+    if(!responseObject.length)
+    {
+        res.sendStatus(500)
+    }
+    const currency = req.query.currency.toUpperCase()
+    if(currency === "USD")
     {
         res.status(200).json(responseObject)
-
     }
     else
     {
-        res.sendStatus(500)
+        const price = await exchangeRat(currency)
+        const responseObjectWithCurrency = responseObject.map(x=>{
+            return{
+                id:x.id,
+                name:x.name,
+                currentPrice:x.currentPrice * price,
+                priceChange:x.priceChange * price,
+                percentPriceChange:x.percentPriceChange,
+                sparkline:x.sparkline.map(x=>x*price)
+            }
+        })
+        res.status(200).json(responseObjectWithCurrency)
+
     }
 }   
 
 export default getETF
-
-//Dodac konwersje walut
